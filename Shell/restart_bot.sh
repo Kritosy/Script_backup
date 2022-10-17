1b9631f34b217561bd97e5cfa3e8591ce306765b
Kritosy <49424130+Kritosy@users.noreply.github.com>
2022-10-17 10:23:40 +0800

    Update restart_bot.sh

--- Shell/restart_bot.sh
+++ Shell/restart_bot.sh
@@ -8,9 +8,9 @@ if [ $botPid ]; then
 else
         echo "stoped... ready to start"
 fi
-#创建screen
+##创建screen
 screen -dm bot
 
-#在screen中执行命令
-#需要执行的命令最好都放在另外一个脚本中执行
+##在screen中执行命令
+##需要执行的命令最好都放在另外一个脚本中执行
 screen -x -S bot -p 0 -X stuff "bash start_bot.sh\n"
